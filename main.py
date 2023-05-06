110df5249b4806c51ad0ac61d6459ccb690ccf95
“yuanhaozuogenius” <yuanhaozuogenius@gmail.com>
2023-05-06 17:09:08 +0800

    PCA , t-SNE

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from src import tSNE
 
 
 def print_hi(name):
@@ -12,5 +13,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    sample_data, df_labels = tSNE.tSNE.load_data()
+    tSNE.tSNE.origin_plot(sample_data, df_labels)
+    # tSNE.PCA.PCA_method(sample_data, df_labels)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
